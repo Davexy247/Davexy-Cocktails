a655aa081642e96e392d58b8372a4c9377bc945b
Davexy <115135444+Davexy247@users.noreply.github.com>
2025-12-25 01:54:29 +0100

    add Hero component with GSAP animations and styling

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,15 @@ import gsap from "gsap";
 import { ScrollTrigger } from "gsap/all";
 import { SplitText } from "gsap/all";
 import Navbar from "./components/Navbar.jsx";
+import Hero from "./components/Hero.jsx";
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
 const App = () => {
   return (
     <main>
       <Navbar />
+      <Hero />
+      <div className="h-dvh bg-black" />
     </main>
   );
 };
